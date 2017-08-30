6ad4b1f85e97e971f4933fbbda06b78886df8218
Jon Bertelsen <jonb@kea.dk>
2017-08-30 14:21:22 +0200

    Fjernet overflødigt output fra programmet

--- DiceGame/DiceGame.java
+++ DiceGame/DiceGame.java
@@ -7,6 +7,8 @@ public class DiceGame
          int weWin = 0;
          int systemWin = 0;
    
+         // Slå 50 gange med terningerne og se fordelingen
+         
          for (int i=0;i<50;i++)
          {
    
@@ -32,15 +34,6 @@ public class DiceGame
       die2 = r.nextInt(6) + 1;
       
       sum = die1 + die2;
-      
-      System.out.println("Terning 1: " + die1);
-      System.out.println("Terning 2: " + die2);
-      System.out.println("Sum: " + sum);
-      
-      if (sum == 7)
-         System.out.println("You win!!!");
-      else
-         System.out.println("System wins!!!"); 
          
       return sum;
    
